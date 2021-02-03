0eabaf8fa3be078359b4417067d4918397bd414b
xiaocheng20 <xiaocheng20@github.com>
2021-02-03 18:40:05 +0800

    use miui camera

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Inherit from msm8996-common
 $(call inherit-product, device/xiaomi/msm8996-common/msm8996.mk)
 
+#Miui Camera
+$(call inherit-product-if-exists, $(LOCAL_PATH)/miuicamera/miuicamera.mk)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
